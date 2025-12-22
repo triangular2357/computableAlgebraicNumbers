f7b50014e3bb066488a75630cea88f4cd15c63ba
L <71787934+triangular2357@users.noreply.github.com>
2025-12-22 18:50:33 +0100

    ListPolynomial

---
+++ ComputableAlgebraicNumbers/ListPolynomial.lean
@@ -0,0 +1,72 @@
+import Mathlib
+
+@[ext] structure Polynom (R : Type*) [Ring R] where
+  coeficients : List R
+--TODO disallow tailing zeros
+--TODO use this structure
+
+def add (R : Type*) [Ring R] : List R → List R → List R
+  | []     , bs      => bs
+  | as     , []      => as
+  | a :: as, b :: bs => (a+b) :: (add R as bs)
+
+def smul (R : Type*) [Ring R] : R → List R → List R
+  | _,      [] => []
+  | r, a :: as => (r*a) :: (smul R r as)
+
+def ℤdiv : ℤ  → List ℤ → List ℤ
+  | _,      [] => []
+  | r, a :: as => (a/r) :: (ℤdiv r as)
+
+def ℚdiv : ℚ  → List ℚ → List ℚ
+  | _,      [] => []
+  | r, a :: as => (a/r) :: (ℚdiv r as)
+
+def mul (R : Type*) [Ring R] : List R → List R → List R
+  | _      , []       => []
+  | []     , _        => []
+  | a :: [], b :: []  => [(a*b)]
+  | a :: as, b :: bs  => (a*b) :: add R (add R (smul R b as) (smul R a bs)) (0 :: mul R as bs)
+
+def ℤgcd : List ℤ → ℕ
+  | [] => 0
+  | r::[]=> r.natAbs
+  | r::rs=> r.gcd (ℤgcd rs)
+
+def ℚlcdm : List ℚ → ℕ --least common denominator multiple
+  | [] => 1
+  | r::[]=> r.den
+  | r::rs=> r.den.lcm (ℚlcdm rs)
+
+def ℤnormalize (i : List ℤ):List ℤ :=
+  ℤdiv ((i.getLastD 1).sign * ℤgcd i) i --last element must not be zero
+
+def ℚnormalize (i : List ℚ):List ℚ := ℚdiv (i.getLastD 1) i
+
+def ℤℚconvert : List ℤ → List ℚ
+  | [] => []
+  | z::zs=> z :: ℤℚconvert zs
+
+def ℚℤmulConvert : ℤ → List ℚ → List ℤ
+  | _, [] => []
+  | x,z::zs=> (z*x).num / (z*x).den  :: ℚℤmulConvert x zs
+
+def ℚℤConvert (i : List ℚ) : List ℤ :=
+  ℚℤmulConvert (ℚlcdm i) i
+
+#eval mul ℤ [1]           [3]
+#eval mul ℤ [1,4]         [3]
+#eval mul ℤ [1,4]       [3,4]
+#eval mul ℤ [1,2,3,4]   [5,7]
+#eval add ℤ [1,4]         [3]
+#eval smul ℤ 2          [1,4]
+#eval ℤdiv   2 [4,2,0,-1,1,4]
+#eval ℤnormalize   [4,2,0,4]
+#eval ℤnormalize   [4,2,0]
+#eval ℤnormalize   [-6]
+#eval ℚdiv   2 [4,2,0,-1,1,4]
+#eval ℚnormalize [4,2,0,-1,1,4]
+#eval ℚℤConvert (ℚnormalize [4,2,0,-1,1,4])
+
+
+#min_imports
