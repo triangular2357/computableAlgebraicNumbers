52d130447240c2289c3a0ad38397b0c6367fecc2
L <71787934+triangular2357@users.noreply.github.com>
2025-12-23 15:53:18 +0100

    refactor CPoly

--- ComputableAlgebraicNumbers/CPoly.lean
+++ ComputableAlgebraicNumbers/CPoly.lean
@@ -3,34 +3,39 @@ import Mathlib.Tactic
 
 namespace CPolynomial
 
-def noLeadingZero {R : Type*} [Ring R] : List R → Prop
+
+private def _noLeadingZero {R : Type*} [Ring R] : List R → Prop
   | []     => True
   | x :: _ => x ≠ 0
 
+def noTailingZero {R : Type*} [Ring R] (i : List R) : Prop :=
+  _noLeadingZero i.reverse
+
 structure CPoly (R : Type*) [Ring R] where
   coefs : List R
-  norm : noLeadingZero coefs.reverse
+  norm : noTailingZero coefs
 
-def removeLeadingZeros' {R : Type*} [Ring R] [DecidableEq R] : List R → List R
+private def _removeLeadingZeros {R : Type*} [Ring R] [DecidableEq R] : List R → List R
   | []      => []
-  | x :: xs => if x = 0 then removeLeadingZeros' xs else x :: xs
+  | x :: xs => if x = 0 then _removeLeadingZeros xs else x :: xs
 
-lemma noLeadingZero_removeLeadingZeros {R : Type*} [Ring R] [DecidableEq R]
-    (l : List R) : noLeadingZero (removeLeadingZeros' l) := by
+private lemma _noLeadingZero_removeLeadingZeros {R : Type*} [Ring R] [DecidableEq R]
+    (l : List R) : _noLeadingZero (_removeLeadingZeros l) := by
   induction l with
   | nil => trivial
   | cons x xs ih =>
-    unfold removeLeadingZeros'
-    rw [apply_ite noLeadingZero]
+    unfold _removeLeadingZeros
+    rw [apply_ite _noLeadingZero]
     by_cases h : x = 0
     · rwa [if_pos h]
     · rwa [if_neg h]
 
-def removeLeadingZeros {R : Type*} [Ring R] [DecidableEq R]
+def removeTailingZeros {R : Type*} [Ring R] [DecidableEq R]
     (l : List R) : CPoly R :=
-  ⟨(removeLeadingZeros' l.reverse).reverse, by
+  ⟨(_removeLeadingZeros l.reverse).reverse, by
+    unfold noTailingZero
     rw [List.reverse_reverse]
-    apply noLeadingZero_removeLeadingZeros
+    apply _noLeadingZero_removeLeadingZeros
   ⟩
 
 end CPolynomial
