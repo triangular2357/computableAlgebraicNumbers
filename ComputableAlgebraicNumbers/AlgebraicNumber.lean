9eceebd75e56935cc5f2284119f619ab1f9bb39e
florianflorianofficial <florianflorianofficial@web.de>
2026-02-02 20:30:24 +0100

    proved that Prealgebraic Numbers define a unique real number and fixed a bug in the definition of prealgebraic number

--- ComputableAlgebraicNumbers/AlgebraicNumber.lean
+++ ComputableAlgebraicNumbers/AlgebraicNumber.lean
@@ -9,7 +9,100 @@ structure PreRealAlgebraicNumber where
   upper : ℚ
   lower_le_upper : lower ≤ upper
   ivt_condition : min_poly.eval lower * min_poly.eval upper ≤ 0
-  deriv_nzero : ∀ x ∈ Set.Icc lower upper, (min_poly.deriv.liftTo ℝ).eval x ≠ 0
+  deriv_nzero : ∀ x ∈ Set.Icc ↑lower ↑upper, (min_poly.deriv.liftTo ℝ).eval x ≠ 0
+
+#check nonneg_of_mul_nonneg_left
+
+lemma PreRealAlgebraicNumber.has_root (a : PreRealAlgebraicNumber) :
+  ∃ x ∈ Set.Icc ↑a.lower ↑a.upper, (a.min_poly.liftTo ℝ).eval x = 0 := by
+  have := intermediate_value_uIcc (a := ↑a.lower) (b := ↑a.upper)
+    (f := (a.min_poly.liftTo ℝ).eval) <| by
+    simp_rw [funext (CPoly.toPolynomial_liftTo_eval_eq_eval₂ (S := ℝ) a.min_poly)]
+    apply Continuous.continuousOn
+    apply Polynomial.continuous_eval₂
+  have := @this 0 <| by
+    apply Set.mem_Icc.2
+    simp_rw [funext (CPoly.toPolynomial_liftTo_eval_eq_eval₂ (S := ℝ) a.min_poly),
+      ← Polynomial.eval_map, show ∀ r, ↑r = (algebraMap ℚ ℝ) r from fun _ ↦ rfl,
+      Polynomial.eval_map_apply, ← show ∀ r, ↑r = (algebraMap ℚ ℝ) r from fun _ ↦ rfl,
+      ← CPoly.toPolynomial_eval]
+    norm_cast
+    by_cases h : 0 < a.min_poly.eval a.lower
+    · constructor
+      · apply min_le_of_right_le
+        apply nonpos_of_mul_nonpos_right a.ivt_condition h
+      · apply le_max_of_le_left (le_of_lt h)
+    · rw [not_lt] at h
+      constructor
+      · apply min_le_of_left_le h
+      · by_cases h' : a.min_poly.eval a.lower = 0
+        · apply le_max_of_le_left (le_of_eq h'.symm)
+        · apply le_max_of_le_right
+          apply nonneg_of_mul_nonpos_right a.ivt_condition
+          apply lt_of_le_of_ne h h'
+  rwa [← Set.uIcc_of_le (Rat.cast_le.2 a.lower_le_upper)]
+
+noncomputable def PreRealAlgebraicNumber.toReal (a : PreRealAlgebraicNumber) : ℝ :=
+  a.has_root.choose
+
+lemma PreRealAlgebraicNumber.toReal_mem_Icc (a : PreRealAlgebraicNumber) :
+  a.toReal ∈ Set.Icc ↑a.lower ↑a.upper :=
+  a.has_root.choose_spec.1
+
+lemma PreRealAlgebraicNumber.toReal_isRoot (a : PreRealAlgebraicNumber) :
+  (a.min_poly.liftTo ℝ).eval a.toReal = 0 :=
+  a.has_root.choose_spec.2
+
+#check Rat.intCast_inj.not.2
+#check Real.ratCast_lt
+
+
+lemma PreRealAlgebraicNumber.uniqueRoot (a : PreRealAlgebraicNumber) (y : ℝ)
+  (h₁ : y ∈ Set.Icc ↑a.lower ↑a.upper) (h₂ : (a.min_poly.liftTo ℝ).eval y = 0) :
+  y = a.toReal := by
+  by_cases h : a.lower = a.upper
+  · have := a.toReal_mem_Icc
+    rw [h, Set.mem_Icc] at h₁ this
+    trans ↑a.upper <;> apply le_antisymm
+    · exact h₁.2
+    · exact h₁.1
+    · exact this.1
+    · exact this.2
+  · by_contra h'
+    have := exists_deriv_eq_zero (f := (a.min_poly.liftTo ℝ).eval) (a := min y a.toReal)
+      (b := max y a.toReal) (min_lt_max.2 h') <| by
+      simp_rw [funext (CPoly.toPolynomial_liftTo_eval_eq_eval₂ (S := ℝ) a.min_poly)]
+      apply Continuous.continuousOn
+      apply Polynomial.continuous_eval₂
+    obtain ⟨c, hc, h'c⟩ := this <| by
+      by_cases h'' : y ≤ a.toReal
+      · rw [min_eq_left h'', max_eq_right h'', h₂, a.toReal_isRoot]
+      · rw [min_eq_right (le_of_not_ge h''), max_eq_left (le_of_not_ge h''), h₂, a.toReal_isRoot]
+    have := a.toReal_mem_Icc
+    apply a.deriv_nzero c
+    · simp only [Set.mem_Ioo, Set.mem_Icc] at hc h₁ this ⊢
+      constructor
+      · calc
+          _ ≤ min y a.toReal := by
+            apply le_min h₁.1 this.1
+          _ ≤ _ := le_of_lt hc.1
+      · calc
+          _ ≤ max y a.toReal := le_of_lt hc.2
+          _ ≤ _ := by
+            apply max_le h₁.2 this.2
+    · simp_rw [funext (CPoly.toPolynomial_liftTo_eval_eq_eval₂ (S := ℝ) a.min_poly.deriv),
+        CPoly.toPolynomial_deriv, ← Polynomial.eval_map, ← Polynomial.derivative_map,
+        ← Polynomial.deriv, Polynomial.eval_map, ]
+      simp_rw [funext (CPoly.toPolynomial_liftTo_eval_eq_eval₂ (S := ℝ) a.min_poly)] at h'c
+      assumption
+
+lemma PreRealAlgebraicNumber.existsUnique_root (a : PreRealAlgebraicNumber) :
+  ∃! x ∈ Set.Icc ↑a.lower ↑a.upper, (a.min_poly.liftTo ℝ).eval x = 0 := by
+  use a.toReal, a.has_root.choose_spec
+  intro y ⟨h, h'⟩
+  exact a.uniqueRoot y h h'
+
+#print ExistsUnique
 
 open ApproximationType
 
@@ -29,7 +122,7 @@ instance : ApproximationType PreRealAlgebraicNumber where
       lower_le_upper := hbetween.1
       ivt_condition := h
       deriv_nzero := fun x hx ↦ a.deriv_nzero x <|
-        Set.Icc_subset_Icc (le_refl a.lower) hbetween.2 hx
+        Set.Icc_subset_Icc (Rat.cast_le.2 (le_refl a.lower)) (Rat.cast_le.2 hbetween.2) hx
     } else {
       min_poly := a.min_poly
       squarefree := a.squarefree
@@ -46,7 +139,7 @@ instance : ApproximationType PreRealAlgebraicNumber where
           ring_nf at this ⊢
           assumption
       deriv_nzero := fun x hx ↦ a.deriv_nzero x <|
-        Set.Icc_subset_Icc hbetween.1 (le_refl a.upper) hx
+        Set.Icc_subset_Icc (Rat.cast_le.2 hbetween.1) (Rat.cast_le.2 (le_refl a.upper)) hx
     }
 
 abbrev intervalLength (p : PreRealAlgebraicNumber) : ℚ := p.upper - p.lower
